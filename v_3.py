328db01d1486b28356581b1a992b1e9aa813120f
st19225 <79070007+st19225@users.noreply.github.com>
2021-08-17 09:11:33 +1200

    Add files via upload

---
+++ v_3.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from v_1 import *
+
+root = Tk()
+
+root.title("OSC Course Selection")
+root.geometry("650x380")
+
+gui = Inter_face(root)
+
+gui.inter_face()
+root.mainloop()
